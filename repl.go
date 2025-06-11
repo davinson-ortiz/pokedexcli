6c2858093ae77a395abc5e70e1c84405977de068
Davinson Ortiz <davinson.ortiz.rivas@gmail.com>
2025-06-10 20:21:43 -0500

    REPL logic started

--- repl.go
+++ repl.go
@@ -1,8 +1,23 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			userInput := scanner.Text()
+			cleanedInput := cleanInput(userInput)
+			fmt.Printf("Your command was: %s\n", cleanedInput[0])
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
